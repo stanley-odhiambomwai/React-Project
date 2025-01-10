c462855cca48161562e6536d317055ae7e6808c5
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-01-10 17:59:38 +0300

    initial commit

---
+++ src/Product.js
@@ -0,0 +1,10 @@
+
+const product = {
+    name: "Wireless Headphones",
+    price: "$99.99",
+    description: "High-quality wireless headphones with noise-cancellation.",
+    image: "https://images.app.goo.gl/ZXKwF8D3KPi3LkZw6"
+  };
+  
+  export default product;
+  
